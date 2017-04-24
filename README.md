8342da3909d2ece9e7ba688313b235780fc83b54
Dan Finlay <dan@danfinlay.com>
2017-04-24 15:08:25 -0700

    Version 2.0.0 allows specifying the module to clone

--- README.md
+++ README.md
@@ -11,7 +11,11 @@ npm install -g ethjs-template
 
 ## Usage
 
-`ethjs-template ethjs-your-module-name`
+```
+ethjs-template ETHJS_MODULE_TO_CLONE NEW_MODULE_NAME
+```
+
+`ethjs-template ethjs-signer ethjs-your-module-name`
 
 This will create a folder named `your-module-name` in the current folder, clone and install `ethjs-signer` inside of it, change all the names to your module's name, install the dependencies, and then run `npm init` to ask you about any other changes to make.
 
--- clone.sh
+++ clone.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 # First argument is the module name:
-mkdir $1
-cd $1
+mkdir $2
+cd $2
 git init
 
 # Use ethjs-signer as template.
-git remote add ethjs-signer-template git@github.com:ethjs/ethjs-signer.git
-git fetch ethjs-signer-template
-git checkout ethjs-signer-template/master
+git remote add ethjs-template git@github.com:ethjs/$1.git
+git fetch ethjs-template
+git checkout ethjs-template/master
 
 # Remove its dist, and replace its name:
 rm -f dist/*
-replace 'ethjs-signer' $1 ./* -r
+replace $1 $2 ./* -r
 
 # Create your own branch, and re-init it:
 git checkout master
